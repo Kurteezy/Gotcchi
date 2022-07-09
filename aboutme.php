cf5672669ab4e8d0d603234be5e1e393b8e7ceaa
Kurteezy <100117236+Kurteezy@users.noreply.github.com>
2022-07-10 02:01:15 +0800

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,65 @@
+<style>
+    img{
+    border-radius: 50%;
+    display: flex;
+  margin-left: auto;
+  margin-right: auto;
+        margin-top: 5%;
+
+    }
+    h3{
+    text-align: center;
+        font-family: monospace;
+    font-size: 30px;
+        font-weight: bold;
+    font-family: "Roboto", sans-serif;
+        color: hotpink;
+        
+    }
+</style>
+<div id="default">
+    <div style="width: 30%; float:left">
+<div id="aboutme">
+    <h3>MY BABY!</h3>
+<img src="images/gotcchi.jpeg" width="300" height="300">
+    <br><br>
+    <p1 class="bio"> 
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Born: March, 22, 2003
+    </p1>
+    <br><br>
+    <p1 class="bio"> 
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Age: 19
+    </p1>
+    <br><br>
+    <p1 class="bio"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Address: Heart ni Kurt<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p1>
+    
+</div>
+    </div>
+    <div style="width: 70%; float:left; height:100%">
+<h3 style="text-align: left;">WHO IS SHE</h3>
+        <hr>
+        <p1 style="color: hotpink; font-size: 25px;">My baby gotcchi is a Blue Scholar at the National University of Calamba (super proud kabag here :P)</p1>
+        <br><br>
+        <p1 style="color: hotpink; font-size: 25px;">She was a member of the LPU Pirates Dance troupe (wowz) and also a member of the NU dance troupe now(galing mo baby)</p1>
+        <br><br><br>
+    <h3 style="text-align: left;">FUN (OR NOT SO FUN) FACTS ABOUT HER</h3>
+        <hr>
+        <ul style="list-style-type:disc; font-size: 25px; color: hotpink">
+  <li>She finds validation through academics.</li>
+  <li>She's allergic to chocolate and shrimp.</li>
+  <li>She is very magastos -_-</li>
+  <li>She loves food! especially peach mango pies, lumpiang togue, sushi, and tudings.</li>
+  <li>She is very mapagbigay :).  </li>
+  <li>She sucks with directions.</li>
+  <li>She gets mad and disappointed at a guy named Kurt a lot.(can't blame her)</li>
+  <li>She absolutely loves coffee!</li>
+  <li>She is very family oriented!</li>
+  <li>She deserves to be treated better.</li>
+  <li>A guy named Kurt does not want to lose her.</li>
+  <li>Her eyesight is bad.</li>
+    <li>She misses her Mama :( </li>        
+</ul>
+<br><br>
+    </div>    
+</div>
---
+++ simple-like-button-codevember.markdown
@@ -0,0 +1,7 @@
+Simple Like Button #codevember
+------------------------------
+
+
+A [Pen](https://codepen.io/emilandersson/pen/rWwvwq) by [Emil Andersson](https://codepen.io/emilandersson) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/emilandersson/pen/rWwvwq/license).
